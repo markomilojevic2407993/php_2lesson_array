7faf3f81b8d191a5a47ca2421c7bd59c15cd6dd6
markomilojevic2407993 <markomilojevic2407993@gmail.com>
2023-05-19 21:44:59 +0200

    bootstrep

--- domaci.php
+++ domaci.php
@@ -8,7 +8,7 @@ $nav = [
 ];
 
 
-
+$godina=date("Y");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,17 +16,29 @@ $nav = [
         <title><?php echo $title; ?></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
         <link href="css/style.css" rel="stylesheet">
     </head>
     <body>
-        <h1><?php echo $title; ?></h1>
-        <nav>
+        <div class="container">
+            <div class="row">
+                
+            
+        <h1 class="text-center"><?php echo $title; ?></h1>
+        <nav class="d-flex justify-content-center bg-primary" >
             <?php foreach ($nav as $navA=>$navLink): ?>
-            <a href="<?php echo $navLink; ?>">
+            <a class="text-decoration-none h3 text-warning text-center p-4" href="<?php echo $navLink; ?>">
               <?php echo $navA; ?>
             </a>
             <?php endforeach; ?>
         </nav>
+            </div>
+        </div>
     
+        <footer class="d-flex justify-content-center bg-dark text-white p-4 h5">
+            copyright &copy;Marko Milojevic <?php echo $godina ; ?>
+        </footer>
     </body>
 </html>
